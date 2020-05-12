1f199e005414622a65a391d1070cf23f6e47cfce
marciovz <marciovz@yahoo.com.br>
2020-05-12 20:46:34 -0300

    Implementado os estilos globais da aplicaçao

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => {
-  return <h1>Page APP</h1>;
+  return (
+    <>
+      <h1>Page APP</h1>
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
