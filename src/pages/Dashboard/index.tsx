215cb91bbe16ccb14279a2aa8dbddc810087e7ae
marciovz <marciovz@yahoo.com.br>
2020-06-14 16:50:47 -0300

    Implemantado estrutura e estilização da listagem de agendamentos na página Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import {FiPower} from 'react-icons/fi';
+import React, { useState } from 'react';
+import {FiPower, FiClock} from 'react-icons/fi';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/Auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth()
 
   return (
@@ -32,6 +45,100 @@ const Dashboard: React.FC = () => {
       </HeaderContent>
 
     </Header>
+
+    <Content>
+      <Schedule>
+        <h1>Horários agendados</h1>
+        <p>
+          <span>Hoje</span>
+          <span>Dia 06</span>
+          <span>Segunda-feira</span>
+        </p>
+
+        <NextAppointment>
+          <strong>Atendimanto a seguir</strong>
+          <div>
+            <img
+            src="https://avatars2.githubusercontent.com/u/12899169?s=460&u=b1212e21ab90d27d67670d6678a37409cab858f2&v=4"
+            alt="Marciovz"
+            />
+
+            <strong>Diego Fernandes</strong>
+            <span>
+              <FiClock />
+              08:00
+            </span>
+          </div>
+        </NextAppointment>
+
+        <Section>
+          <strong>Manhã</strong>
+
+          <Appointment>
+            <span>
+              <FiClock />
+              08:00
+            </span>
+
+            <div>
+              <img
+              src="https://avatars2.githubusercontent.com/u/12899169?s=460&u=b1212e21ab90d27d67670d6678a37409cab858f2&v=4"
+              alt="Marciovz"
+              />
+            </div>
+          </Appointment>
+
+          <Appointment>
+            <span>
+              <FiClock />
+              08:00
+            </span>
+
+            <div>
+              <img
+              src="https://avatars2.githubusercontent.com/u/12899169?s=460&u=b1212e21ab90d27d67670d6678a37409cab858f2&v=4"
+              alt="Marciovz"
+              />
+            </div>
+          </Appointment>
+        </Section>
+
+        <Section>
+          <strong>Tarde</strong>
+
+          <Appointment>
+            <span>
+              <FiClock />
+              08:00
+            </span>
+
+            <div>
+              <img
+              src="https://avatars2.githubusercontent.com/u/12899169?s=460&u=b1212e21ab90d27d67670d6678a37409cab858f2&v=4"
+              alt="Marciovz"
+              />
+            </div>
+          </Appointment>
+
+          <Appointment>
+            <span>
+              <FiClock />
+              08:00
+            </span>
+
+            <div>
+              <img
+              src="https://avatars2.githubusercontent.com/u/12899169?s=460&u=b1212e21ab90d27d67670d6678a37409cab858f2&v=4"
+              alt="Marciovz"
+              />
+            </div>
+          </Appointment>
+        </Section>
+
+
+      </Schedule>
+      <Calendar></Calendar>
+    </Content>
   </Container>
   );
 };
